97057164816173a7293eca9eb05d673691768935
Yuriy Nemtsov <georgecalm@gmail.com>
2012-12-01 21:22:06 -0500

    ensuring /test /test/ and /test/index.html all work

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ app = connect()
   .use('/js/lib/', connect.static('node_modules/requirejs/'))
   .use('/node_modules', connect.static('node_modules'))
   .use('/test', connect.static('test/'))
-  .use('/test', connect.static('app'))
   ;
 
 http.createServer(app).listen(8080, function() {
